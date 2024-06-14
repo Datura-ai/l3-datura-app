cc9912b5cf3bca3951d1e0860f41c0e00a407f9a
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2024-06-14 18:02:53 +0400

    feat: create credential service

---
+++ apps/ui/src/services/credential/useCredentialService.ts
@@ -0,0 +1,35 @@
+import { useQuery, useMutation } from '@apollo/client'
+import { ToastContext } from 'contexts'
+import { useContext } from 'react'
+
+import createCredentialGql from 'gql/credential/createCredential.gql'
+import { CredentialInput } from 'types/credential'
+
+
+export const useCreateCredentialService = () => {
+    const { setToast } = useContext(ToastContext)
+    const [mutation, { loading }] = useMutation(createCredentialGql)
+
+    const createCredential = async (input: CredentialInput) => {
+        try {
+            const {
+                data: { createCredential },
+            } = await mutation({
+                variables: {
+                    input: input,
+                },
+            })
+
+            return createCredential
+        } catch (error) {
+            setToast({
+                message: error?.message ?? 'Error creating Credential',
+                type: 'negative',
+                open: true,
+            })
+            throw error; // Rethrow the error to propagate it to the caller
+        }
+    }
+
+    return { createCredential, loading }
+}
