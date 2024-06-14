a45e03ba55da67885a9e01f1f581d15726b3b931
levanion <levanion1998@gmail.com>
2024-06-14 17:49:32 +0400

    feat: secrets page title & description

--- apps/ui/src/pages/Secrets/Secrets.tsx
+++ apps/ui/src/pages/Secrets/Secrets.tsx
@@ -2,43 +2,45 @@ import { StyledChatWrapper, StyledContainer, StyledMainWrapper } from 'routes/Ch
 import { StyledAppContainer } from 'components/Layout/LayoutStyle'
 import { StyledTableWrapper } from 'pages/Billing/panels/Transactions'
 import styled from 'styled-components'
-import { StyledHeaderGroup } from 'pages/Home/homeStyle.css'
+import {
+  StyledHeaderGroup,
+  StyledSectionDescription,
+  StyledSectionTitle,
+} from 'pages/Home/homeStyle.css'
 import { ButtonPrimary } from 'components/Button/Button'
 import Button from 'share-ui/components/Button/Button'
 import Table from 'components/Table'
 import useSecret from './useSecret'
 
 const Secrets = () => {
-    const { columns, handleCreateSecret, fetch_secret_loading, secrets } = useSecret()
+  const { columns, handleCreateSecret, fetch_secret_loading, secrets } = useSecret()
 
-    return (
-        <StyledAppContainer>
-            <StyledContainer>
-                <StyledMainWrapper>
-                    <StyledChatWrapper>
-                        <StyledTabsWrapper>
-                            <StyledHeaderGroup className='header_group'>
-                                <ButtonPrimary
-                                    onClick={handleCreateSecret}
-                                    size={Button.sizes?.MEDIUM}
-                                >
-                                    Create Secret
-                                </ButtonPrimary>
-                            </StyledHeaderGroup>
-                        </StyledTabsWrapper>
+  return (
+    <StyledAppContainer>
+      <StyledContainer>
+        <StyledMainWrapper>
+          <StyledChatWrapper>
+            <StyledHeaderGroup className='header_group'>
+              <div>
+                <StyledSectionTitle>Secrets</StyledSectionTitle>
+                <StyledSectionDescription>
+                  Manage and configure secrets used across your applications securely.
+                </StyledSectionDescription>
+              </div>
 
-                        <StyledTableWrapper>
-                            <Table 
-                                columns={columns} 
-                                data={secrets} 
-                                isLoading={fetch_secret_loading} 
-                            />
-                        </StyledTableWrapper>
-                    </StyledChatWrapper>
-                </StyledMainWrapper>
-            </StyledContainer>
-        </StyledAppContainer>
-    )
+              <ButtonPrimary onClick={handleCreateSecret} size={Button.sizes?.MEDIUM}>
+                Create Secret
+              </ButtonPrimary>
+            </StyledHeaderGroup>
+
+            <StyledTableWrapper>
+              <Table columns={columns} data={secrets} isLoading={fetch_secret_loading} />
+            </StyledTableWrapper>
+          </StyledChatWrapper>
+        </StyledMainWrapper>
+      </StyledContainer>
+    </StyledAppContainer>
+  )
 }
 
 export default Secrets
