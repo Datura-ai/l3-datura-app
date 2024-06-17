e9c3fb5d3d7026f6f244b0c44b2b9c67c9b74952
levanion <levanion1998@gmail.com>
2024-06-17 15:20:06 +0400

    fix: template list render condition

--- apps/ui/src/pages/template/TemplateLayout.tsx
+++ apps/ui/src/pages/template/TemplateLayout.tsx
@@ -11,30 +11,35 @@ import { useNavigate, useOutlet } from 'react-router-dom'
 import Box from '@mui/material/Box'
 
 import TemplateList from './TemplateList'
+import useTemplate from './useTemplate'
 
 const TemplateLayout = () => {
   const navigate = useNavigate()
   const outlet = useOutlet()
 
+  const { templates } = useTemplate()
+
   return (
     <StyledAppContainer>
       <StyledContainer>
         <StyledMainWrapper>
-          <StyledLeftColumn customWidth={400}>
-            <Box
-              display={'flex'}
-              flexDirection={'column'}
-              sx={{ paddingRight: 1.5 }}
-              position={'relative'}
-            >
-              <ListHeader
-                title={'Templates'}
-                onAddClick={() => navigate('/templates/create-template')}
-              />
+          {templates.length > 0 && (
+            <StyledLeftColumn customWidth={400}>
+              <Box
+                display={'flex'}
+                flexDirection={'column'}
+                sx={{ paddingRight: 1.5 }}
+                position={'relative'}
+              >
+                <ListHeader
+                  title={'Templates'}
+                  onAddClick={() => navigate('/templates/create-template')}
+                />
 
-              <TemplateList />
-            </Box>
-          </StyledLeftColumn>
+                <TemplateList />
+              </Box>
+            </StyledLeftColumn>
+          )}
 
           <StyledChatWrapper>{outlet}</StyledChatWrapper>
         </StyledMainWrapper>
